c45f54413144f238a772bf9d6a4085648986d0ec
PartiNoone <tracymv@uci.edu>
2025-04-23 22:33:38 +0000

    added set to is_valid to try to filter out duplicate urls

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import re
 from urllib.parse import urlparse
 from lxml import etree
 from bs4 import BeautifulSoup
+import json
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -12,43 +13,69 @@ def scraper(url, resp):
 # Detect and avoid infinite traps
 # Detect and avoid sets of similar pages with no information
 # Detect and avoid dead URLs that return a 200 status but no data
-def extract_next_links(url:str, resp: utils.response.Response): -> list
+
+# main focus: extract links
+def extract_next_links(url:str, resp):
     # Implementation required.
     # url: the URL that was used to get the page
     # resp.url: the actual url of the page
-    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
+    # resp.status: the status code. 200 is OK, page served. Other numbers mean that there was some kind of problem.
     # resp.error: when status is not 200, you can check the error here, if needed.
     # resp.raw_response:
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page
-    soup = BeautifulSoup(resp.raw_response.content,'lxml')
-    links = [link.get('href') for link in soup.find_all('a')]
-    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    # only include links if they pass is_valid
-    links = [url if is_valid(url) for url in links]
-    return links
+    if resp.status == 200:
+        soup = BeautifulSoup(resp.raw_response.content,'lxml')
+        links = [link.get('href') for link in soup.find_all('a')]
+        # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+        # only include links if they pass is_valid
+        # DEBUG THINGS---------------------------------------------------------------------
+        # print(f"Currently extracting links from {url}.")
+        # print(f"Links found:")
+        for link in links:
+            print(f"\t{link}")
+        for i in range(len(links) - 1, -1,-1):
+            if not is_valid(links[i]):
+                links.pop(i)
+        # DEBUG THINGS---------------------------------------------------------------------
+        # print(f"Valid links:")
+        for link in links:
+            print(f"\t{link}")
+        return links
+    return list()
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
-    # There are already some conditions that return False.
     # Add things to it to help crawler avoid traps/loops/etc.
-        # Only crawl these?:
+        # Only crawl these:
         # *.ics.uci.edu/* netloc
         # *.cs.uci.edu/* netloc
         # *.informatics.uci.edu/* netloc
         # *.stat.uci.edu/* netloc
-        # today.uci.edu/department/information_computer_sciences/* netloc + part of the path
-        # such as https://ics.uci.edu/academics/undergraduate-programs/ , where netloc matches
-        # vision.ics.uci.edu/ where part of the netloc matches
+        # today.uci.edu/department/information_computer_sciences/*
+        # Ex. such as https://ics.uci.edu/academics/undergraduate-programs/
+        # Ex. vision.ics.uci.edu/
     try:
         parsed = urlparse(url)
         # url parses into scheme://netloc/path;parameters?query#fragment
         # we want to ignore fragments when counting unique pages
         if parsed.scheme not in set(["http", "https"]):
             return False
-        # add more specifications here
-        return not re.match(
+
+        # add more specifications here? re.match(substring, string)
+        if not re.match(
+            # .* means any combination and number of characters
+            # no $ sign because we want to allow a path
+            r".*(ics.uci.edu|cs.uci.edu|informatics.uci.edu"
+            + r"|stat.uci.edu|today.uci.edu/department/information_computer_sciences)"
+            , (parsed.netloc + '/' + parsed.path).lower()):
+            return False
+
+        if re.match( # returns the first occurrence of a substring in the string and ignore others
+            # .* means any combination and number of characters
+            # \. means a dot
+            # $ means end of string; so "jpeg" matches "me.jpeg" but not "me.jpeg2"
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
@@ -56,8 +83,16 @@ def is_valid(url):
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()):
+            return False
 
+        # Add the url to a set
+        with open("explored.json", "w+") as setfile:
+            urls = json.load(setfile)
+            urls.add(url)
+            setfile.dump(urls)
+
+        return True
     except TypeError:
         print ("TypeError for ", parsed)
         raise
