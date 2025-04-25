5678c0c51894286b4b20b3db1e11a84937d24995
PartiNoone <tracymv@uci.edu>
2025-04-25 07:31:17 +0000

    added a filter to avoid going through individual calendar months

--- scraper.py
+++ scraper.py
@@ -66,6 +66,7 @@ def is_valid(url): # TODO: MAYBE CHANGE THE SIGNATURE TO TAKE IN A RESPONSE OR T
         if re.match(
             # filter out unwanted pages
             r".*(isg.ics.uci.edu/events/tag/talk/day"              # individual calendar days
+            + r"|isg.ics.uci.edu/events/tag/talk/20"               # individual calendar months
             + r"|isg.ics.uci.edu/events/tag/talk/list"             # individual calendar days
             + r"|intranet.ics.uci.edu/doku.php$"                   # requires login
             + r"|intranet.ics.uci.edu/doku.php/personnel:start"    # requires login
